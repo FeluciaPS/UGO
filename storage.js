8be51061143e7314d538d9e1e466ad800c926923
TheMezStrikes <=>
2021-02-08 19:31:10 +0100

    add storage.js

---
+++ storage.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const FS = require('fs');
+
+let ioStatus = {};
+const PATH = 'storage';
+
+if (!FS.existsSync(PATH)) FS.mkdir(PATH);
+
+module.exports = {
+    save: function(filename, data) {
+        if (ioStatus[filename]) return setTimeout(this.save, 500, filename, data);
+		ioStatus[filename] = true;
+        FS.writeFile(`${PATH}/temp-${filename}`, JSON.stringify(data, null, 2), () => {
+			FS.rename(`${PATH}/temp-${filename}`, `${PATH}/${filename}`, () => {
+				delete ioStatus[filename];
+			})
+		})
+    },
+    load: function(filename) {
+        if (!FS.existsSync(`${PATH}/${filename}`)) return {};
+        return JSON.parse(FS.readFileSync(`${PATH}/${filename}`));
+    }
+}
