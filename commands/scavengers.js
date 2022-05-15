121aeb54c083ca0ba98abd19d1fb8bbefc61dfc3
FeluciaPS <TheMezStrikes@gmail.com>
2022-05-15 17:07:22 +0200

    apparently old code just didn't work

--- commands/scavengers.js
+++ commands/scavengers.js
@@ -2,33 +2,49 @@ let {unescape} = require('html-escaper');
 
 let lastHunt = false;
 
-// Code by PartMan, I don't understand the majority of it, not my fault if it doesn't work
-let parseScavsHunt = function (data) {
-    // Hunts
-    if (/^<div class="broadcast-blue"><strong>The (?:regular|official|practice|recycled|unrated|mini) scavenger hunt by [^<>]*? was ended/.test(data)) {
-        // Hunt ended
-        let [res] = data.split('<details style="cursor: pointer;">');
-        let intro = res.match(/The (.*?) scavenger hunt by (.*?) was ended/);
-        let huntType = intro[1], huntmaker = intro[2];
-        let finishers = [], questions = [], hunt = {};
 
-        let t = res.match(/<em>.*?<\/em> <span style="color: lightgreen;">\[(?:\d{2}:){0,2}\d{2}:\d{2}\]<\/span>/g)
-		if (t) t.forEach(hit => {
-			let usermatch = hit.match(/(?<=<em>).*?(?=<\/em>)/)
-			if (usermatch) usermach = usermatch[0];
-            let user = toId(unescape(usermatch));
-            let time = hit.match(/(?<=>)\[(?:\d{2}:){0,2}\d{2}:\d{2}\](?=<)/)
-			if (time) time = time[0].split(':').reverse().reduce((acc, val, index) => acc + ~~toId(val) * [1, 60, 60, 24].slice(0, index + 1).reduce((a, b) => a * b, 1), 0);
-            finishers.push({ user: user, time: time * 1000 });
-        });
-
-        hunt.type = huntType;
-        hunt.maker = huntmaker;
-        hunt.finishers = finishers;
-        return hunt;
-    }
-    return false;
+let parseScavsHunt = function(data) {
+	let huntEnd = data.match(/^<div class="broadcast-blue"><strong>The (?:(?:regular|official|practice|recycled|unrated|mini) )?[sS]cavenger [hH]unt (?:by ((?=<em>)<em>.*<\/em>|.*) )?was ended/);
+	let hunt = {};
+	if (huntEnd) {
+		let [res] = split[2].split('<details style="cursor: pointer;">');
+		let makerText = res.match(/The (.*?) ?[sS]cavenger [hH]unt (?:by (.*?) )?was ended/)[2];
+		let tempMaker = hunt.makers;
+		hunt.makers = (makerText || '').match(/(?<=<em>).+?(?=<\/em>)/g)
+		if (hunt.makers) hunt.makers = hunt.makers.map(match => toID(tools.unescapeHTML(match))) || [toID(tools.unescapeHTML(makerText))];
+		if (makerText && makerText.includes(' and ') && timestamp < 1621755253153n) {
+			hunt.makers = makerText.split(' and ').map(t => t.split(',')).flat().map(u => toID(tools.unescapeHTML(u)));
+		}
+		hunt.finishers = [];
+		if (!hunt.makers.join('')) {
+			hunt.makers = tempMaker;
+			(res.match(/(?<=\d(?:st|nd|rd|th) place: |Consolation Prize: )((?:<em>)?[^<]+?(?:<\/em>)?(?:<span style="[^"]+?">\[[\d:]+\]<\/span>)?)+(?=<br)/g) || []).forEach(match => {
+				match.split(', ').forEach(m => {
+					let time = (m.match(/(?<=<span style="color: [^"]+">\[)((?:\d{2}:)?\d{2}:\d{2})(?=\])/));
+					if (time) time = (time[0].split(':').map(n => ~~n).reverse().reduce((acc, val, index) => acc + val * [1, 60, 60, 24].slice(0, index + 1).reduce((a, b) => a * b, 1), 0) * 1000) || undefined;
+					if (time) m = m;
+					let user = toID(tools.unescapeHTML(m.replace(/<span.*<\/span>/g, '').replace(/<[^>]*>/g, '')));
+					hunt.finishers.push({ user: user, time: time });
+				});
+			});
+		}
+		else {
+			let x = res.match(/<em>.*?<\/em> <span style="color: lightgreen;">\[(?:\d{2}:){0,2}\d{2}:\d{2}\]<\/span>/g)
+			if (x) x.forEach(hit => {
+				let m = hit.match(/(?<=<em>).*?(?=<\/em>)/);
+				let user = toID(unescape(m[0]));
+				let time = hit.match(/(?<=>)\[(?:\d{2}:){0,2}\d{2}:\d{2}\](?=<)/)
+				if (time) time = time[0].split(':').reverse().reduce((acc, val, index) => acc + ~~toID(val) * [1, 60, 60, 24].slice(0, index + 1).reduce((a, b) => a * b, 1), 0);
+				else time = 0;
+				hunt.finishers.push({ user: user, time: time * 1000 });
+			});
+		}
+		return hunt;
+	}
+	return false;
 }
+
+// Code by PartMan, I don't understand the majority of it, not my fault if it doesn't work
 
 bot.on('raw', (parts) => {
 	let room = Rooms[Utils.getRoom(parts[0])];
